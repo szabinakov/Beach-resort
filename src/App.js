ce9de34591556c0b8e73c36abbc228ef06ec94dd
szabinakov <szabinkov@gmail.com>
2020-11-27 23:02:01 +0000

    Did Routing, Set up Navbar

--- src/App.js
+++ src/App.js
@@ -1,8 +1,26 @@
-// import logo from "./logo.svg";
-// import './App.css';
+import "./App.css";
+
+import Home from "./pages/Home";
+import Rooms from "./pages/Rooms";
+import SingleRoom from "./pages/SingleRooms";
+import Error from "./pages/Error";
+
+import Navbar from "./components/Navbar";
+
+import { Route, Switch } from "react-router-dom";
 
 function App() {
-  return <>hello frodm app</>;
+  return (
+    <>
+      <Navbar />
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/rooms/" component={Rooms} />
+        <Route exact path="/rooms/:slug" component={SingleRoom} />
+        <Route component={Error} />
+      </Switch>
+    </>
+  );
 }
 
 export default App;
