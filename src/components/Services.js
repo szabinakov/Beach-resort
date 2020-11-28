b372f9def6ccfacde55dc8c68208062d872f3ca2
szabinakov <szabinkov@gmail.com>
2020-11-28 12:36:12 +0000

    added Title, Banner, Services components

---
+++ src/components/Services.js
@@ -0,0 +1,52 @@
+import React, { Component } from "react";
+import Title from "./Title";
+import { FaCocktail, FaHiking, FaShuttleVan, FaBeer } from "react-icons/fa";
+
+export default class Services extends Component {
+  state = {
+    services: [
+      {
+        icon: <FaCocktail />,
+        title: "free cocktails ",
+        info:
+          "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Temporibus, tempore!",
+      },
+      {
+        icon: <FaHiking />,
+        title: "free hiking ",
+        info:
+          "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Temporibus, tempore!",
+      },
+      {
+        icon: <FaShuttleVan />,
+        title: "free shuttlevan ",
+        info:
+          "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Temporibus, tempore!",
+      },
+      {
+        icon: <FaBeer />,
+        title: "free beer ",
+        info:
+          "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Temporibus, tempore!",
+      },
+    ],
+  };
+  render() {
+    return (
+      <section className="services">
+        <Title title="services" />
+        <div className="services-center">
+          {this.state.services.map((item, index) => {
+            return (
+              <article key={index} className="service">
+                <span>{item.icon}</span>
+                <h6>{item.title}</h6>
+                <p>{item.info}</p>
+              </article>
+            );
+          })}
+        </div>
+      </section>
+    );
+  }
+}
