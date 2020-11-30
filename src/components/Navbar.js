0a59ab19cc21b64a8edc4445bff37162ee0c5992
szabinakov <szabinkov@gmail.com>
2020-11-30 15:36:22 +0000

    swapped the Navbar class component to functional component

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,42 +1,37 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import { FaAlignRight } from "react-icons/fa";
 import logo from "../images/logo.svg";
-export default class Navbar extends Component {
-  state = {
-    isOpen: false,
+
+export default function Navbar() {
+  const [originalState, setOriginalState] = useState({ isOpen: false });
+
+  const handleToggle = () => {
+    setOriginalState({ isOpen: !originalState.isOpen });
   };
-  handleToggle = () => {
-    this.setState({ isOpen: !this.state.isOpen });
-  };
-  render() {
-    return (
-      <nav className="navbar">
-        <div className="nav-center">
-          <div className="nav-header">
-            <Link to="/">
-              <img src={logo} alt="Beach Resort" />
-            </Link>
-            <button
-              type="button"
-              className="nav-btn"
-              onClick={this.handleToggle}
-            >
-              <FaAlignRight className="nav-icon" />
-            </button>
-          </div>
-          <ul
-            className={this.state.isOpen ? "nav-links show-nav" : "nav-links"}
-          >
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/rooms">Rooms</Link>
-            </li>
-          </ul>
+
+  return (
+    <nav className="navbar">
+      <div className="nav-center">
+        <div className="nav-header">
+          <Link to="/">
+            <img src={logo} alt="Beach Resort" />
+          </Link>
+          <button type="button" className="nav-btn" onClick={handleToggle}>
+            <FaAlignRight className="nav-icon" />
+          </button>
         </div>
-      </nav>
-    );
-  }
+        <ul
+          className={originalState.isOpen ? "nav-links show-nav" : "nav-links"}
+        >
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/rooms">Rooms</Link>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
 }
