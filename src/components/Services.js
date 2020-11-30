14c5be24f658e253c4695be576aa10fb782f1b2e
szabinakov <szabinkov@gmail.com>
2020-11-30 15:32:18 +0000

    as a little exercise I have swapped the Services class component to functional component

--- src/components/Services.js
+++ src/components/Services.js
@@ -1,9 +1,9 @@
-import React, { Component } from "react";
+import React from "react";
 import Title from "./Title";
 import { FaCocktail, FaHiking, FaShuttleVan, FaBeer } from "react-icons/fa";
 
-export default class Services extends Component {
-  state = {
+export default function Services() {
+  const state = {
     services: [
       {
         icon: <FaCocktail />,
@@ -31,22 +31,21 @@ export default class Services extends Component {
       },
     ],
   };
-  render() {
-    return (
-      <section className="services">
-        <Title title="services" />
-        <div className="services-center">
-          {this.state.services.map((item, index) => {
-            return (
-              <article key={index} className="service">
-                <span>{item.icon}</span>
-                <h6>{item.title}</h6>
-                <p>{item.info}</p>
-              </article>
-            );
-          })}
-        </div>
-      </section>
-    );
-  }
+
+  return (
+    <section className="services">
+      <Title title="services" />
+      <div className="services-center">
+        {state.services.map((item, index) => {
+          return (
+            <article key={index} className="service">
+              <span>{item.icon}</span>
+              <h6>{item.title}</h6>
+              <p>{item.info}</p>
+            </article>
+          );
+        })}
+      </div>
+    </section>
+  );
 }
